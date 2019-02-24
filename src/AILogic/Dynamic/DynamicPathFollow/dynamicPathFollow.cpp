d574443ebe9a5bb80ac69405b5b6c0822eedcca6
NotMickey <mukulkakroo18@gmail.com>
2019-02-23 19:45:43 -0700

    added logic for dynamic path follow

---
+++ src/AILogic/Dynamic/DynamicPathFollow/dynamicPathFollow.cpp
@@ -0,0 +1,36 @@
+#include "dynamicPathFollow.h"
+
+#include "../DynamicArrive/dynamicArrive.h"
+
+AIProject::DynamicPathFollow::DynamicPathFollow(Boid & i_character, const float & i_maxSpeed, const float & i_maxAcceleration,
+										const float & i_slowRadius, const float & i_targetRadius, const std::vector<ofVec2f>& i_waypoints) :
+	m_character(i_character), m_maxSpeed(i_maxSpeed), m_maxAcceleration(i_maxAcceleration), m_slowRadius(i_slowRadius),
+	m_targetRadius(i_targetRadius), m_waypoints(i_waypoints)
+{}
+
+AIProject::DynamicSteeringOutput AIProject::DynamicPathFollow::GetSteering()
+{
+	DynamicSteeringOutput output;
+
+	if (!m_waypoints.empty())
+	{
+		ofVec2f targetPosition = m_waypoints[m_waypoints.size() - 1];
+
+		float distance = m_character.m_kinematic.position.distance(targetPosition);
+
+		if (distance < m_targetRadius)
+		{
+			m_waypoints.pop_back();
+			targetPosition = m_waypoints[m_waypoints.size() - 1];
+		}
+
+		Kinematic target;
+		target.position = targetPosition;
+
+		DynamicArrive arrive(m_character, target, m_maxAcceleration, m_maxSpeed, m_targetRadius, m_slowRadius, 0.7f);
+
+		output = arrive.GetSteering();
+	}
+
+	return output;
+}
---
+++ src/AILogic/Dynamic/DynamicPathFollow/dynamicPathFollow.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "../../steeringBase.h"
+#include "../../../Character/boid.h"
+
+#include <vector>
+
+namespace AIProject
+{
+	class DynamicPathFollow : public SteeringBase
+	{
+	public:
+		DynamicPathFollow(Boid &i_character, const float &i_maxSpeed, const float &i_maxAcceleration, const float &i_slowRadius,
+							const float &i_targetRadius, const std::vector<ofVec2f> &i_waypoints);
+		
+		DynamicSteeringOutput GetSteering();
+
+	private:
+		float m_maxSpeed;
+		float m_maxAcceleration;
+		float m_slowRadius;
+		float m_targetRadius;
+
+		Boid & m_character;
+
+		std::vector<ofVec2f> m_waypoints;
+
+	};
+}
