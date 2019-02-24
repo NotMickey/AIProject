88a0436116d021b59374aa07caacb69e90b9632f
NotMickey <mukulkakroo18@gmail.com>
2019-02-24 16:54:07 -0700

    Added logic for stationary obstacle avoidance

---
+++ src/AILogic/Dynamic/ObstacleAvoidance/obstacleAvoidance.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "../../steeringBase.h"
+#include "../../../Character/boid.h"
+
+#include "../../../Physics/CollisionDetection/collisionDetection.h"
+
+namespace AIProject
+{
+	class ObstacleAvoidance : public SteeringBase
+	{
+	public:
+		ObstacleAvoidance(Boid &i_character, const float &i_maxAcceleration, const Physics::CollisionDetection &i_collisionDetector, const float &i_avoidDistance,
+								const float &i_lookAheadDistance);
+
+		DynamicSteeringOutput GetSteering();
+
+	private:
+		float m_maxAcceleration;
+		float m_avoidDistance;
+		float m_lookAheadDistance;
+
+		Physics::CollisionDetection m_collisionDetector;
+
+		Boid m_character;
+	};
+}
