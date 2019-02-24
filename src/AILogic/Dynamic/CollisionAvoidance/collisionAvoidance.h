71524df956a96b2595abed2ebc7d9506c60a3e0f
NotMickey <mukulkakroo18@gmail.com>
2019-02-24 08:25:08 -0700

    Added logic for collision avoidance

---
+++ src/AILogic/Dynamic/CollisionAvoidance/collisionAvoidance.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../../steeringBase.h"
+#include "../../../Character/boid.h"
+
+namespace AIProject
+{
+	class CollisionAvoidance : public SteeringBase
+	{
+	public:
+		CollisionAvoidance(Boid &i_character, Boid i_targets[], const int &i_size, const float &i_radiusOfAvoidance, const float &i_angleOfAvoidance);
+
+		DynamicSteeringOutput GetSteering();
+
+	private:
+		int m_arraySize;
+
+		Boid * m_targets;
+
+		float m_radiusOfAvoidance;
+		float m_angleOfAvoidance;
+
+		Boid m_character;
+
+		bool CanCollide(const Boid &i_target);
+	};
+
+}
