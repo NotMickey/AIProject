b4972ecfc5c36f8e077dbff13c2fed0b7e99a2eb
NotMickey <mukulkakroo18@gmail.com>
2019-02-24 20:09:05 -0700

    created the tile map class (needs testing)

--- src/Graph/TileMap/tileMap.cpp
+++ src/Graph/TileMap/tileMap.cpp
@@ -1,7 +1,172 @@
 #include "tileMap.h"
 
-AIProject::Graph::TileMap::TileMap(const int & i_totalLength, const int & i_tileLength, const ofVec2f &i_startPosition) :
-						m_totalLength(i_totalLength), m_tileLength(i_tileLength), m_startPosition(i_startPosition)
+AIProject::Graph::TileMap::TileMap(const int & i_numOfTilesPerSide, const int & i_tileLength, const ofVec2f &i_startPosition) :
+						m_numOfTilesPerSide(i_numOfTilesPerSide), m_tileLength(i_tileLength), m_startPosition(i_startPosition)
 {
-	m_numOfTiles = i_totalLength / i_tileLength;
+	BuildGraph();
+}
+
+AIProject::Graph::DirectedWeightedGraph AIProject::Graph::TileMap::GetGraph()
+{
+	return m_graph;
+}
+
+int AIProject::Graph::TileMap::GetNodeFromMouseClick(const ofVec2f &i_position)
+{
+	if (i_position.x < m_startPosition.x || i_position.x > m_startPosition.x + m_numOfTilesPerSide * m_tileLength)
+		return -1;
+
+	if (i_position.y < m_startPosition.y || i_position.y > m_startPosition.y + m_numOfTilesPerSide * m_tileLength)
+		return -1;
+
+	float xDist = i_position.x - m_startPosition.x;
+	
+	int column = 0;
+
+	while (column * m_tileLength < xDist)
+	{
+		if (m_tileLength * (column + 1) > xDist)
+			break;
+
+		column++;
+	}
+
+	float yDist = i_position.y - m_startPosition.y;
+
+	int row = 0;
+
+	while (row * m_tileLength < yDist)
+	{
+		if (m_tileLength * (row + 1) > xDist)
+			break;
+
+		row++;
+	}
+		
+	// At this point I have the row and column numbers
+
+	int nodeID = m_numOfTilesPerSide * row + column;
+
+	return nodeID;
+}
+
+void AIProject::Graph::TileMap::BuildGraph()
+{
+	AddNodes();
+
+	AddEdges();
+}
+
+void AIProject::Graph::TileMap::AddNodes()
+{
+	float tileHalfLength = m_tileLength / 2;
+
+	for (int i = 0; i < m_numOfTilesPerSide - 1; i++)
+	{
+		for (int j = 0; j < m_numOfTilesPerSide - 1; j++)
+		{
+			ofVec2f position;
+			position.y = m_startPosition.y + m_numOfTilesPerSide * i + tileHalfLength;
+			position.x = m_startPosition.x + m_numOfTilesPerSide * j + tileHalfLength;
+
+			m_graph.AddNode(position);
+		}
+	}
+}
+
+void AIProject::Graph::TileMap::AddEdges()
+{
+	for (int i = 0; i < m_numOfTilesPerSide - 1; i++)
+	{
+		for (int j = 0; j < m_numOfTilesPerSide - 1; i++)
+		{
+			if (IsEdgeTile(i, j))
+			{
+				if (IsCornerTile(i, j))
+				{
+					if (i == 0)
+					{
+						m_graph.AddEdge(i, i + 1, m_edgeCost);
+
+						if (j == 0)
+							m_graph.AddEdge(j, j + 1, m_edgeCost);
+						else
+							m_graph.AddEdge(j, j - 1, m_edgeCost);
+					}
+					else
+					{
+						m_graph.AddEdge(i, i - 1, m_edgeCost);
+
+						if (j == 0)
+							m_graph.AddEdge(j, j + 1, m_edgeCost);
+						else
+							m_graph.AddEdge(j, j - 1, m_edgeCost);
+					}
+				}
+				else
+				{
+					if (j > 0 && j < m_numOfTilesPerSide - 1)
+					{
+						m_graph.AddEdge(j, j + 1, m_edgeCost);
+						m_graph.AddEdge(j, j - 1, m_edgeCost);
+
+						if (i == 0)
+							m_graph.AddEdge(i, i + 1, m_edgeCost);
+						else
+							m_graph.AddEdge(i, i - 1, m_edgeCost);
+					}
+					else
+					{
+						m_graph.AddEdge(i, i + 1, m_edgeCost);
+						m_graph.AddEdge(i, i - 1, m_edgeCost);
+
+						if (j == 0)
+							m_graph.AddEdge(j, j + 1, m_edgeCost);
+						else
+							m_graph.AddEdge(j, j - 1, m_edgeCost);
+					}
+				}
+			}
+			else
+			{
+				m_graph.AddEdge(i, i + 1, m_edgeCost);
+				m_graph.AddEdge(i, i - 1, m_edgeCost);
+
+				m_graph.AddEdge(j, j + 1, m_edgeCost);
+				m_graph.AddEdge(j, j - 1, m_edgeCost);
+			}
+		}
+	}
+}
+
+bool AIProject::Graph::TileMap::IsEdgeTile(const int & i_row, const int & i_column)
+{
+	if (i_row == 0 || i_row == m_numOfTilesPerSide - 1)
+		return true;
+
+	if (i_column == 0 || i_column == m_numOfTilesPerSide - 1)
+		return true;
+
+	return false;
+}
+
+bool AIProject::Graph::TileMap::IsCornerTile(const int & i_row, const int & i_column)
+{
+	// top left
+	if (i_row == 0 && i_column == 0)
+		return true;
+
+	// top right
+	if (i_row == 0 && i_column == m_numOfTilesPerSide - 1)
+		return true;
+
+	// bottom left
+	if (i_row == m_numOfTilesPerSide - 1 && i_column == 0)
+		return true;
+
+	// bottom right
+	if (i_row == m_numOfTilesPerSide - 1 && i_column == m_numOfTilesPerSide - 1)
+		return true;
+
+	return false;
 }
--- src/Graph/TileMap/tileMap.h
+++ src/Graph/TileMap/tileMap.h
@@ -9,16 +9,29 @@ namespace AIProject
 		class TileMap
 		{
 		public:
-			TileMap(const int &i_totalLength, const int &i_tileLength, const ofVec2f &i_startPosition);
+			TileMap(const int &i_numOfTilesPerSide, const int &i_tileLength, const ofVec2f &i_startPosition);
+
+			DirectedWeightedGraph GetGraph();
+
+			int GetNodeFromMouseClick(const ofVec2f &i_position);
 
 		private:
-			float m_totalLength;
 			float m_tileLength;
-			float m_numOfTiles;
+			float m_numOfTilesPerSide;
+
+			float m_edgeCost;
 
 			ofVec2f m_startPosition;
 
 			DirectedWeightedGraph m_graph;
+
+			void BuildGraph();
+
+			void AddNodes();
+			void AddEdges();
+
+			bool IsEdgeTile(const int &i_row, const int &i_column);
+			bool IsCornerTile(const int &i_row, const int &i_column);
 		};
 	}
 }
