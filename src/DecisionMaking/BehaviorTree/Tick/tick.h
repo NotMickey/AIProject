5a736fcec43aee57503d450af8cad06f5469e3c2
NotMickey <mukulkakroo18@gmail.com>
2019-04-02 21:35:09 -0600

    Create tick.h

---
+++ src/DecisionMaking/BehaviorTree/Tick/tick.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+
+#include "../../Blackboard/blackboard.h"
+#include "../task.h"
+
+namespace AIProject
+{
+	namespace DecisionMaking
+	{
+		class Tick
+		{
+		public:
+			
+
+		private:
+			// Behavior tree
+			Blackboard & m_blackboard;
+			//std::vector<t>
+		};
+	}
+}
