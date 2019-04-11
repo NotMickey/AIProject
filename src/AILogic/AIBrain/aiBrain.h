6c80a76a5718b2f78d3f81f01ff7ceb520c6606e
NotMickey <mukulkakroo18@gmail.com>
2019-04-11 08:32:15 -0600

    new ai brain added

---
+++ src/AILogic/AIBrain/aiBrain.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../../DecisionMaking/TreeBase/decisionMakingBehavior.h"
+
+namespace AIProject
+{
+	class AIBrain
+	{
+	private:
+		//DecisionMaking::DecisionMakingBehavior* _
+	};
+}
